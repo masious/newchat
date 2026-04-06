c4adcbb8d4bfe4a9f0f073362c991c4a6462f94f
Masoud Bonabı <masoudbonabi@gmail.com>
2026-04-06 13:38:53 -0400

    disable sentry in dev

--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -6,6 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
+  enabled: process.env.NODE_ENV === "production",
   dsn: "https://339dd126caad54176cc1a30c887a3543@o4505723490795520.ingest.us.sentry.io/4511162904477696",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
